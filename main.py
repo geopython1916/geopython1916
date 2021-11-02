ba14d5b6cc277897cc6d1e5aecddf65ec86676e6
Joseph O'Connor <93155070+geopython1916@users.noreply.github.com>
2021-11-01 21:46:01 -0400

    main.py
    Main Menu - Basic Coordinate Geometry Proceedures

---
+++ main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+
+print("\n\n\n")
+print("Basic Plane Geometry Proceedures\n\n")
+print("0 Store X(Easting), Y(Northing), Z(Elevation), Desciption\n")
+print("1 Inverse\n")
+print("2 Traverse\n")
+print("3 Bearing-Bearing Intersection\n")
+print("4 Bearing-Distant Intersection - Clock-wise and Conter-clockwise Solutions\n")
+print("5 Distance-Distance Intersection - Clock-wise and Conter-clockwise Solution\n")
+print("6. Store Parcel\n")
+print("7. Store Alignment\n")
+
+proceedure = input("\nEnter proceedure of next Calculation: \n")
+proceedure = int(proceedure)
+if proceedure == 0:
+    print("0 - Got a true expression value")
+    print (proceedure)
+elif proceedure == 1:
+    print("1 - Got a true expression value")
+    print(proceedure)
+elif proceedure == 2:
+    print("2 - Got a true expression value")
+    print(proceedure)
+elif proceedure == 3:
+    print("3 - Got a true expression value")
+    print (proceedure)    
+elif proceedure == 4:
+    print("4 - Got a true expression value")
+    print(proceedure)
+elif proceedure == 5:
+    print("5 - Got a true expression value")
+    print(proceedure)
+elif proceedure == 6:
+    print("6 - Got a true expression value")
+    print(proceedure)
+elif proceedure == 7:
+    print("7 - Got a true expression value")
+    print(proceedure)
+else:
+    print("Did not match any options")
+    print(proceedure + "\n\n\n")
+
+print("\n\nGood bye!")
+
+
